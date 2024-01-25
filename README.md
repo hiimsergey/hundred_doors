b49a144f9333f5aff2993d5338126f9bf97f6106
Sergey Lavrent <goldstein.siarhei@gmail.com>
2024-01-25 19:22:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# 100 Doors
+> Imagine a hundred closed doors in a corridor.
+> You go and open every one of them.
+> Then, you open every second one. (If the door was open, then just close it.)
+> Then, every third. Then, every fourth and every fifth and so on until you only touch the 100th door.
+> How many doors are open now?
+
+This is a mathematical problem known as "100 Doors". I have written a smol
+program in Rust that calculates and visualizes the solution using
+[ncurses-rs](https://crates.io/crates/ncurses).
+
+```
+A simple program that calculates and visualizes the "100 Doors" problem
+
+Usage: hundred-doors [OPTIONS] [NUMBER]
+
+Arguments:
+  [NUMBER]  How many doors there are [default: 100]
+
+Options:
+  -d, --draw           Whether there should be a graphical representation of these doors
+  -e, --explain        Whether to explain the problem
+      --twave <TWAVE>  How many milliseconds to wait before the next wave [default: 0]
+      --tdoor <TDOOR>  How many milliseconds to wait before doors [default: 0]
+  -h, --help           Print help
+  -V, --version        Print version
+```
+
+## Examples
+```
+$ hundred-doors 100
+> 10
+```
+
+```
+$ hundred-doors 100 --draw --tdoor 10 --twave 50
+```
+
+<center>*ncurses opens*</center>
---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use std::{process, thread, time::Duration};
+use clap::Parser;
+use ncurses::*;
+
+#[derive(Parser)]
+#[command(author, version)]
+#[command(arg_required_else_help(true))]
+/// A simple program that calculates and visualizes the "100 Doors" problem.
+struct Args {
+	/// How many doors there are
+	#[arg(default_value_t = 100)]
+	number: usize,
+
+	/// Whether there should be a graphical representation of these doors
+	#[arg(short, long)]
+	draw: bool,
+
+	/// Whether to explain the problem
+	#[arg(short, long)]
+	explain: bool,
+
+	/// How many milliseconds to wait before the next wave
+	#[arg(long, default_value_t = 0)]
+	twave: u64,
+
+	/// How many milliseconds to wait before doors
+	#[arg(long, default_value_t = 0)]
+	tdoor: u64
+}
+
+const EXPLANATION: &str = "Imagine a hundred closed doors in a corridor.
+You go and open every one of them.
+Then, you open every second one. (If the door was open, then just close it.)
+Then, every third. Then, every fourth and every fifth and so on until you only touch the 100th door.
+You go and open every one of them.
+
+With this program you can set a custom number of doors and have the total number
+and their respective indices printed and/or visualized.";
+
+fn main() {
+    let args = Args::parse();
+
+	if args.explain {
+		println!("{EXPLANATION}");
+		process::exit(0);
+	}
+
+	let mut doors: Vec<bool> = vec![false; args.number];
+
+	if args.draw {
+		draw(&mut doors, args);
+	} else {
+		for wave in 0..args.number {
+			for i in (wave..args.number).step_by(wave + 1) {
+				doors[i] = !doors[i];
+			}
+		}
+	}
+
+	println!("{}", doors.iter().filter(|d| **d).count());
+}
+
+fn draw(doors: &mut [bool], args: Args) {
+    initscr();
+    raw();
+    keypad(stdscr(), true);
+    timeout(0);
+
+	// Register colors, red and green
+	start_color();
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+
+	for wave in 0..args.number {
+		for door in &*doors {
+			// Key codes of Ctrl-C, Esc and 'q'
+			if [3, 27, 113].contains(&getch()) { process::exit(2); }
+
+			addstr(if *door {
+				attron(COLOR_PAIR(1));
+				"open "
+			} else {
+				attron(COLOR_PAIR(2));
+				"shut "
+			});
+			refresh();
+			thread::sleep(Duration::from_millis(args.tdoor));
+		}
+
+		// Door toggling part
+		for i in (wave..args.number).filter(|i| i % (wave + 1) == 0) {
+			doors[i] = !doors[i];
+		}
+
+		thread::sleep(Duration::from_millis(args.twave));
+		clear();
+	}
+
+	attroff(COLOR_PAIR(1));
+	attroff(COLOR_PAIR(2));
+	
+    endwin();
+}
